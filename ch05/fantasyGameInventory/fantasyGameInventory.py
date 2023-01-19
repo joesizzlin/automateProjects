661b21234ddc5dedf6cdeb26207ddfcfddef248a
joesizzlin <cassellajoe@gmail.com>
2023-01-19 18:02:11 -0500

    create inventory dictionary

--- ch05/fantasyGameInventory/fantasyGameInventory.py
+++ ch05/fantasyGameInventory/fantasyGameInventory.py
@@ -1,3 +1,6 @@
 #fantasyGameInventory, pg 120
 # create a function that will take any possible inventory (using dictionary value) and display as user friendly output.
 # starting inventory: 1 rope, 6 torch, 42 gold coin, 1 dagger
+
+inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 12}
+
