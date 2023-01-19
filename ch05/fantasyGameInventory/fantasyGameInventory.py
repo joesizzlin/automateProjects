a6b561b8085a44fa48ea32027b4988c7dfd9c080
joesizzlin <cassellajoe@gmail.com>
2023-01-19 18:19:58 -0500

    finish function, passed test

--- ch05/fantasyGameInventory/fantasyGameInventory.py
+++ ch05/fantasyGameInventory/fantasyGameInventory.py
@@ -1,6 +1,15 @@
 #fantasyGameInventory, pg 120
 # create a function that will take any possible inventory (using dictionary value) and display as user friendly output.
-# starting inventory: 1 rope, 6 torch, 42 gold coin, 1 dagger
+# starting inventory: 1 rope, 6 torch, 42 gold coin, 1 dagger, 12 arrow
 
-inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 12}
+inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
+def displayInventory(stuff):
+    print('Inventory: ')
+    items_total = 0
+    for k,v in inventory.items():
+        print(str(v) + ' ' + k)
+        items_total += v
+    print('Total number of Items: ' + str(items_total))
+
+displayInventory(inventory)
