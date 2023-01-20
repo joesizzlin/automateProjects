d0f73dbbf555d23cceb1a07a1b5203bf71afa123
joesizzlin <cassellajoe@gmail.com>
2023-01-19 20:05:35 -0500

    correct project title

--- ch05/fantasyGameInventory/fantasyGameInventory.py
+++ ch05/fantasyGameInventory/fantasyGameInventory.py
@@ -1,4 +1,4 @@
-#fantasyGameInventory, pg 120
+# Fantasy Game Inventory Project
 # create a function that will take any possible inventory (using dictionary value) and display as user friendly output.
 # starting inventory: 1 rope, 6 torch, 42 gold coin, 1 dagger, 12 arrow
 
