e37a242d1ae0a2bfa429ab7d85e1677eb32b7004
joesizzlin <cassellajoe@gmail.com>
2023-01-19 19:50:32 -0500

    better use of global vs local variables

--- ch05/fantasyGameInventory/fantasyGameInventory.py
+++ ch05/fantasyGameInventory/fantasyGameInventory.py
@@ -4,10 +4,10 @@
 
 inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}              # dictionary for inventory     
 
-def displayInventory(inventory):
+def displayInventory(stuff):
     print('Inventory: ')
     items_total = 0                                         # declare items_total and assign int(0)
-    for k,v in inventory.items():                           # in the for loop, the string of the inventory item and assign to k, assign number of items as v
+    for k,v in stuff.items():                           # in the for loop, the string of the inventory item and assign to k, assign number of items as v
         print(str(v) + ' ' + k)                             # prints the number of items, then the name of the item
         items_total += v                                    # add number of items to our tally of items_total
     print('Total number of Items: ' + str(items_total))
