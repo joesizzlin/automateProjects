2a4b83f42a9e2fdcca44373cf489b3b5ab7053a1
joesizzlin <cassellajoe@gmail.com>
2023-01-19 19:45:30 -0500

    code comments

--- ch05/fantasyGameInventory/fantasyGameInventory.py
+++ ch05/fantasyGameInventory/fantasyGameInventory.py
@@ -2,14 +2,14 @@
 # create a function that will take any possible inventory (using dictionary value) and display as user friendly output.
 # starting inventory: 1 rope, 6 torch, 42 gold coin, 1 dagger, 12 arrow
 
-inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}              # dictionary for inventory     
 
-def displayInventory(stuff):
+def displayInventory(inventory):
     print('Inventory: ')
-    items_total = 0
-    for k,v in inventory.items():
-        print(str(v) + ' ' + k)
-        items_total += v
+    items_total = 0                                         # declare items_total and assign int(0)
+    for k,v in inventory.items():                           # in the for loop, the string of the inventory item and assign to k, assign number of items as v
+        print(str(v) + ' ' + k)                             # prints the number of items, then the name of the item
+        items_total += v                                    # add number of items to our tally of items_total
     print('Total number of Items: ' + str(items_total))
 
-displayInventory(inventory)
+displayInventory(inventory)                                 # takes the inventory dictionary and runs the displayInventory function
