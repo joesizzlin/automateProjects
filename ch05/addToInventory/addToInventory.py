a923a4119d90be79af1befb855e1b17dffd1f09e
joesizzlin <cassellajoe@gmail.com>
2023-01-21 11:15:10 -0500

    dragonLoot comment

--- ch05/addToInventory/addToInventory.py
+++ ch05/addToInventory/addToInventory.py
@@ -8,6 +8,6 @@
 
 # include: 1). inv{} dictionary, 2). dragonLoot[] list, 3). run that dictionary and list through function above and assign as 'inv'
 
-dragonLoot=['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+dragonLoot=['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']            # dragonLoot list
 
 # use displayInventory() from other ch05 project with 'inv' to print user friendly output.
