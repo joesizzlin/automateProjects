d8590a28f985a68a549b56ef20e4b85af5ad2cf3
joesizzlin <cassellajoe@gmail.com>
2023-01-21 11:19:41 -0500

    add inv{} dictionary

--- ch05/addToInventory/addToInventory.py
+++ ch05/addToInventory/addToInventory.py
@@ -7,7 +7,7 @@
 
 
 # include: 1). inv{} dictionary, 2). dragonLoot[] list, 3). run that dictionary and list through function above and assign as 'inv'
-
+inv={'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}          # inv dictionary
 dragonLoot=['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']            # dragonLoot list
 
 # use displayInventory() from other ch05 project with 'inv' to print user friendly output.
